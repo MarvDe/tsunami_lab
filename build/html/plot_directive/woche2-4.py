304da470e39e74e1fca92487b0dca303f3aee1ff
philipp <philipp.prell@icloud.com>
2026-04-29 21:32:24 +0200

    Remove build folder

--- build/html/plot_directive/woche2-4.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-df = pd.read_csv("rh1u2solution_1.csv")
-
-plt.plot(df.iloc[:, 0], df.iloc[:, 2])
-plt.xlabel("X-Position")
-plt.ylabel("Y-Wasserhöhe")
-plt.title("Höhe: 1, Momentum: 2")
