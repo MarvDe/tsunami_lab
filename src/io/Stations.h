196a633e7e12eb4d5d16c6f55ddc9d45ee73a5b9
MarvDe <marvin_doering05@t-online.de>
2026-05-06 19:59:37 +0200

    week 4 completed

--- src/io/Stations.h
+++ src/io/Stations.h
@@ -20,15 +20,13 @@ struct tsunami_lab::io::Station{
     t_real m_posX;
     t_real m_posY;
     std::ostream *m_stream;
+    bool m_ownsStream;
 };
 
 class tsunami_lab::io::Stations{
     private:
         //! array of stations
         std::vector<tsunami_lab::io::Station> m_stations;
-
-        //! array of streams
-        std::vector<std::unique_ptr<std::ofstream>> m_streams;
 
         //! number of cells
         t_idx m_cellX = 0;
@@ -54,6 +52,7 @@ class tsunami_lab::io::Stations{
          */
         ~Stations();
 
+
         /**
          * Adds a station for which data can be tracked.
          *
@@ -61,8 +60,10 @@ class tsunami_lab::io::Stations{
          * @param i_posX position in x direction
          * @param i_posY position in y direction
          * @param io_stream output stream for writing data
+         * @param i_ownsStream flag for memory management if true memory io_stream gets deleted else it is assumed that stream is externally managed.
          **/
-        void addStation(const std::string &i_name, t_real i_posX, t_real i_posY, std::ostream &io_stream);
+        void addStation(const std::string &i_name, t_real i_posX, t_real i_posY, std::ostream *io_stream, bool i_ownsStream = false);
+
 
         /**
          * Writes data to the output stream.
@@ -76,6 +77,7 @@ class tsunami_lab::io::Stations{
          **/
         void write( t_real i_simTime, const t_real * i_height, const t_real * i_momentumX, const t_real * i_momentumY, const t_real * i_bathymetry, t_idx i_stride ) const;
 
+        
         /**
          * Reads station from yaml file
          * 
