7d298214bf2bfae06773f3a4c6e5210715f4d0e1
MarvDe <marvin_doering05@t-online.de>
2026-05-15 20:16:32 +0200

    finished netcdf write

--- src/io/NetCdf.cpp
+++ src/io/NetCdf.cpp
@@ -6,16 +6,10 @@
  **/
 #include "NetCdf.h"
 
-
-void tsunami_lab::io::NetCdf::errorChecking(int i_errId){
-    if (i_errId != NC_NOERR){
-        std::cerr << nc_strerror(i_errId) << std::endl;
-    }
-}
-
-
-void tsunami_lab::io::NetCdf::create(t_idx i_nx, t_idx i_ny, const std::string & i_filePath, NetCdfFile * o_file){
-    o_file = nullptr;
+tsunami_lab::io::NetCdf::NetCdf( t_idx i_nx, t_idx i_ny, t_real i_dxy, t_real i_dt, const std::string & i_filePath ){
+    m_dxy = i_dxy;
+    m_dt = i_dt;
+    // Opening new netcdf file
     // check if file path ends with '.nc'
     if (i_filePath.length() < 3 ||
         i_filePath[i_filePath.length() - 3] != '.' ||
@@ -24,89 +18,93 @@ void tsunami_lab::io::NetCdf::create(t_idx i_nx, t_idx i_ny, const std::string &
             std::cerr << "netcdf file should end with '.nc'" << std::endl;
             return;
     }
+    // creating file
+    errorChecking( nc_create(i_filePath.c_str(), NC_CLOBBER, &m_fileId) );
 
-    int l_fileId;    
-    errorChecking( nc_create(i_filePath.c_str(), NC_CLOBBER, &l_fileId) );
     // creating dimensions
-    int l_xDimId, l_yDimId, l_tDimId; 
-    errorChecking( nc_def_dim(l_fileId, "x", i_nx, &l_xDimId) );
-    errorChecking( nc_def_dim(l_fileId, "y", i_nx, &l_yDimId) );
-    errorChecking( nc_def_dim(l_fileId, "t", NC_UNLIMITED, &l_tDimId) );
+    errorChecking( nc_def_dim(m_fileId, "x", i_nx, &m_xDimId) );
+    errorChecking( nc_def_dim(m_fileId, "y", i_ny, &m_yDimId) );
+    errorChecking( nc_def_dim(m_fileId, "t", NC_UNLIMITED, &m_tDimId) );
 
     // creating variables
-    int l_hVarId, l_huVarId, l_hvVarId, l_bVarId;
-    int l_dimIds[3] = {l_tDimId, l_yDimId, l_xDimId};
-    
-    errorChecking( nc_def_var(l_fileId, "h", NC_FLOAT, 3, l_dimIds, &l_hVarId) );
-    errorChecking( nc_put_att_schar(l_fileId, l_hVarId, "units", NC_UBYTE, 1, "m") );
+    int l_dimIds[3] = {m_tDimId, m_yDimId, m_xDimId};
 
-    errorChecking( nc_def_var(l_fileId, "hu", NC_FLOAT, 3, l_dimIds, &l_huVarId) );
-    errorChecking( nc_put_att_schar(l_fileId, l_hVarId, "units", NC_UBYTE, 6, "kg*m/s") );
+    errorChecking( nc_def_var(m_fileId, "x", NC_FLOAT, 1, &m_xDimId, &m_xVarId) );
+    errorChecking( nc_put_att_text(m_fileId, m_xVarId, "units", 1, "m") );
+
+    errorChecking( nc_def_var(m_fileId, "y", NC_FLOAT, 1, &m_yDimId, &m_yVarId) );
+    errorChecking( nc_put_att_text(m_fileId, m_yVarId, "units", 1, "m") );
+
+    errorChecking( nc_def_var(m_fileId, "t", NC_FLOAT, 1, &m_tDimId, &m_tVarId) );
+    errorChecking( nc_put_att_text(m_fileId, m_tVarId, "units", 1, "s") );
     
-    errorChecking( nc_def_var(l_fileId, "hv", NC_FLOAT, 3, l_dimIds, &l_hvVarId) );
-    errorChecking( nc_put_att_schar(l_fileId, l_hVarId, "units", NC_UBYTE, 6, "kg*m/s") );
+    errorChecking( nc_def_var(m_fileId, "h", NC_FLOAT, 3, l_dimIds, &m_hVarId) );
+    errorChecking( nc_put_att_text(m_fileId, m_hVarId, "units", 1, "m") );
+
+    errorChecking( nc_def_var(m_fileId, "hu", NC_FLOAT, 3, l_dimIds, &m_huVarId) );
+    errorChecking( nc_put_att_text(m_fileId, m_huVarId, "units", 6, "kg*m/s") );
     
-    errorChecking( nc_def_var(l_fileId, "b", NC_FLOAT, 3, l_dimIds, &l_bVarId) );
-    errorChecking( nc_put_att_schar(l_fileId, l_hVarId, "units", NC_UBYTE, 1, "s") );
+    errorChecking( nc_def_var(m_fileId, "hv", NC_FLOAT, 3, l_dimIds, &m_hvVarId) );
+    errorChecking( nc_put_att_text(m_fileId, m_hvVarId, "units", 6, "kg*m/s") );
+    
+    errorChecking( nc_def_var(m_fileId, "b", NC_FLOAT, 3, l_dimIds, &m_bVarId) );
+    errorChecking( nc_put_att_text(m_fileId, m_bVarId, "units", 1, "m") );
     
     // register COARDS convention
     const char *l_coardsStr = "COARDS";
-    nc_put_att_text( l_fileId, NC_GLOBAL, "Conventions", 6, l_coardsStr);
+    nc_put_att_text( m_fileId, NC_GLOBAL, "Conventions", 6, l_coardsStr);
 
-    nc_enddef(l_fileId);
-    *o_file = {l_fileId, l_xDimId, l_yDimId, l_tDimId, l_hVarId, l_huVarId, l_hvVarId, l_bVarId};
+    nc_enddef(m_fileId);
+
+    for (tsunami_lab::t_idx l_cx = 0; l_cx < i_nx; l_cx++){
+        tsunami_lab::t_real l_cxFloat = l_cx * m_dxy;
+        errorChecking( nc_put_var1_float(m_fileId, m_xVarId, &l_cx, &l_cxFloat) );
+    }
+
+    for (tsunami_lab::t_idx l_cy = 0; l_cy < i_ny; l_cy++){
+        tsunami_lab::t_real l_cyFloat = l_cy * m_dxy;
+        errorChecking( nc_put_var1_float(m_fileId, m_yVarId, &l_cy, &l_cyFloat) );
+    }
 }
 
+tsunami_lab::io::NetCdf::~NetCdf(){
+    errorChecking( nc_close(m_fileId) );
+}
 
-void tsunami_lab::io::NetCdf::write(t_real               i_dxy,
-                                    t_idx                i_nx,
+void tsunami_lab::io::NetCdf::errorChecking(int i_errId){
+    if (i_errId != NC_NOERR){
+        std::cerr << nc_strerror(i_errId) << std::endl;
+    }
+}
+
+void tsunami_lab::io::NetCdf::write(t_idx                i_nx,
                                     t_idx                i_ny,
                                     t_idx                i_timeStep,
                                     t_idx                i_stride,
                                     t_real       const * i_h,
                                     t_real       const * i_hu,
                                     t_real       const * i_hv,
-                                    t_real       const * i_bathymetry,
-                                    const NetCdfFile   & i_Ncfile){
+                                    t_real       const * i_bathymetry ){
     
-    int l_fileId = i_Ncfile.m_fileId;
-    int l_hVarId = i_Ncfile.m_hVarId;
-    int l_huVarId = i_Ncfile.m_huVarId;
-    int l_hvVarId = i_Ncfile.m_hvVarId;
-    int l_bVarId = i_Ncfile.m_bVarId;
-
     // write data to file
+    tsunami_lab::t_real l_timeStep = i_timeStep;
+    errorChecking( nc_put_var1_float(m_fileId, m_tVarId, &i_timeStep, &l_timeStep) );
     for (t_idx l_cy = 0; l_cy < i_ny; l_cy++){
         for (t_idx l_cx = 0; l_cx < i_nx; l_cx++){
-            t_idx l_index[3] = {i_timeStep, l_cy, l_cx};
+            t_idx l_index[3] = {i_timeStep * m_dt, l_cy * m_dxy, l_cx * m_dxy};
             if (i_h != nullptr){
-                errorChecking( nc_put_var1_float( l_fileId, l_hVarId, l_index, &i_h[l_cy * i_stride + l_cx] ) );
+                errorChecking( nc_put_var1_float( m_fileId, m_hVarId, l_index, &i_h[l_cy * i_stride + l_cx] ) );
             }
             if (i_hu != nullptr){
-                errorChecking( nc_put_var1_float( l_fileId, l_huVarId, l_index, &i_hu[l_cy * i_stride + l_cx] ) );
+                errorChecking( nc_put_var1_float( m_fileId, m_huVarId, l_index, &i_hu[l_cy * i_stride + l_cx] ) );
             }
             if (i_hv != nullptr){
-                errorChecking( nc_put_var1_float( l_fileId, l_hvVarId, l_index, &i_hv[l_cy * i_stride + l_cx] ) );
+                errorChecking( nc_put_var1_float( m_fileId, m_hvVarId, l_index, &i_hv[l_cy * i_stride + l_cx] ) );
             }
             if (i_bathymetry != nullptr){
-                errorChecking( nc_put_var1_float( l_fileId, l_bVarId, l_index, &i_bathymetry[l_cy * i_stride + l_cx] ) );
+                errorChecking( nc_put_var1_float( m_fileId, m_bVarId, l_index, &i_bathymetry[l_cy * i_stride + l_cx] ) );
             }
         }
     }
 }
 
-
-void tsunami_lab::io::NetCdf::readBathymetry( tsunami_lab::t_idx     i_cellX,
-                                              tsunami_lab::t_idx     i_cellY,
-                                              const std::string    & i_filePath,
-                                              tsunami_lab::t_real  * o_bathymetry){
-    
-}
-
-
-void tsunami_lab::io::NetCdf::readDisplacement( tsunami_lab::t_idx     i_cellX,
-                                                tsunami_lab::t_idx     i_cellY,
-                                                const std::string    & i_filePath,
-                                                tsunami_lab::t_real  * o_displacement){
-
-}
