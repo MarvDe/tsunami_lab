e61257dddbd29ddbe70c35d5cbb191021be39453
Philipp Prell <philipp.prell@icloud.com>
2026-05-15 18:42:34 +0200

    implemented readBathymetry, added tests

--- src/io/NetCdf.cpp
+++ src/io/NetCdf.cpp
@@ -33,3 +33,35 @@ void io::NetCdf::readDisplacement(  t_idx,
 
     nc_close(l_ncid);
 }
+
+void io::NetCdf::readBathymetry(t_idx,
+                                t_idx,
+                                std::string i_filePath,
+                                t_real * o_bathymetry){
+    int l_ncid;
+    int l_status;
+                                    
+    l_status = nc_open(i_filePath.c_str(), NC_NOWRITE, &l_ncid);
+
+    if (l_status != NC_NOERR){
+        printf("Fehler beim Öffnen: %s\n", nc_strerror(l_status));
+        return;
+    }
+
+    int l_varidZ;
+
+    l_status = nc_inq_varid(l_ncid, "z", &l_varidZ);
+    if (l_status != NC_NOERR){
+        printf("Variable nicht gefunden: %s\n", nc_strerror(l_status));
+        return;
+    }
+
+
+    l_status = nc_get_var_float(l_ncid, l_varidZ, o_bathymetry);
+    if (l_status != NC_NOERR){
+        printf("Read error %s\n", nc_strerror(l_status));
+        return;
+    }
+
+    nc_close(l_ncid);
+}
